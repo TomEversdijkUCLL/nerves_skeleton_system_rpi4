1b99cc94bac7e1bc700c9326d3268e808aa568db
Tom Eversdijk <tom.eversdijk@telenet.be>
2022-02-16 09:29:16 +0100

    Try adding python imutils as dependecy

---
+++ package/python-imutils/python-imutils.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# python-imutils
+#
+################################################################################
+
+PYTHON_IMUTILS_VERSION = 0.5.4
+PYTHON_IMUTILS_SOURCE = imutils-$(PYTHON_IMUTILS_VERSION).tar.gz
+PYTHON_IMUTILS_SITE = https://files.pythonhosted.org/packages/3f/d3/ecb4d108f6c1041d24842a345ee0123cd7f366ba75cf122601e856d42ba2
+PYTHON_IMUTILS_SETUP_TYPE = distutils
+
+$(eval $(python-package))
